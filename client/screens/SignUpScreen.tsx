dfec74dd3e2c29dae94542b66581bf2a102988cd
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-26 18:55:55 -0400

    updated signup screen

--- client/screens/SignUpScreen.tsx
+++ client/screens/SignUpScreen.tsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button, TextInput } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const SignUpScreen = ({ navigation }) => {
 	return (
-		<View>
-			<Text>Hello</Text>
+		<View className="flex-1 justify-center items-center space-y-10">
+			<View className="container mx-auto px-4">
+				<TextInput label="Name" />
+				<TextInput label="Email" />
+				<TextInput label="Password" />
+				<TextInput label="Repeat Password" secureTextEntry right={<TextInput.Icon icon="eye" />} />
+			</View>
+			<Button>Sign In</Button>
+			<Text>Or</Text>
+			<Button>Sign up with Google</Button>
 		</View>
 	);
 };
