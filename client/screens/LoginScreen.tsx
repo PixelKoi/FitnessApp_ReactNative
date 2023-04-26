b8b5aa1cc7a9f6869db701a58354cff5b7ef658c
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-26 18:40:50 -0400

    updated loginscreen tags

--- client/screens/LoginScreen.tsx
+++ client/screens/LoginScreen.tsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 export default class LoginScreen extends Component {
 	render() {
 		return (
-			<View>
-				<Text> textInComponent </Text>
+			<View className="flex-1 justify-center items-center space-y-10">
+				<TextInput> Name </TextInput>
+				<TextInput> Password </TextInput>
+				<Button>Login</Button>
+				<Text>Forgot your password?</Text>
+				<Text>or</Text>
+				<Button>Continue With Google</Button>
 			</View>
 		);
 	}
