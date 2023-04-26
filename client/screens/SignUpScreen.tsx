ab1b8ec6be0abe05a0144ecf0c13ede62cb83b43
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-26 18:58:17 -0400

    updated signup screen

--- client/screens/SignUpScreen.tsx
+++ client/screens/SignUpScreen.tsx
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const SignUpScreen = ({ navigation }) => {
+	const [ name, setName ] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ checkPass, setCheckPass ] = useState('');
+
 	return (
 		<View className="flex-1 justify-center items-center space-y-10">
 			<View className="container mx-auto px-4">
 				<TextInput label="Name" />
 				<TextInput label="Email" />
-				<TextInput label="Password" />
+				<TextInput label="Password" secureTextEntry right={<TextInput.Icon icon="eye" />} />
 				<TextInput label="Repeat Password" secureTextEntry right={<TextInput.Icon icon="eye" />} />
 			</View>
-			<Button>Sign In</Button>
+			<Button mode="contained">Sign In</Button>
 			<Text>Or</Text>
-			<Button>Sign up with Google</Button>
+			<Button mode="contained">Sign up with Google</Button>
 		</View>
 	);
 };
