448703ba9f11ea721fd98f0d04d448f133783c9b
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-26 18:31:09 -0400

    updated navigation on home screen

--- client/screens/HomeScreen.tsx
+++ client/screens/HomeScreen.tsx
@@ -7,11 +7,11 @@ const HomeScreen = ({ navigation }) => {
 		<View className="flex-1 justify-center items-center space-y-10">
 			<Text className="text-4xl"> Fitness PR </Text>
 			<View className="container mx-auto px-4">
-				<Button className="mb-4 py-2 " sonPress={() => navigation.navigate('SignUp')} mode="contained">
+				<Button className="mb-4 py-2" onPress={() => navigation.navigate('SignUp')} mode="contained">
 					<Text>Sign Up</Text>
 				</Button>
-				<Button className="py-2 " mode="contained">
-					<Text onPress={() => navigation.navigate('Login')}>Login</Text>
+				<Button className="py-2" onPress={() => navigation.navigate('Login')} mode="contained">
+					<Text>Login</Text>
 				</Button>
 			</View>
 		</View>
