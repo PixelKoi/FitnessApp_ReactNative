f54c30c848c32bee7257d773b6b4a8eb7bcd6d76
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-26 18:45:51 -0400

    updated login screen

--- client/screens/LoginScreen.tsx
+++ client/screens/LoginScreen.tsx
@@ -6,12 +6,18 @@ export default class LoginScreen extends Component {
 	render() {
 		return (
 			<View className="flex-1 justify-center items-center space-y-10">
-				<TextInput> Name </TextInput>
-				<TextInput> Password </TextInput>
-				<Button>Login</Button>
+				<View className="container mx-auto px-4">
+					<TextInput className="mb-5"> Name </TextInput>
+					<TextInput> Password </TextInput>
+				</View>
+				<Button className="" mode="contained">
+					<Text>Login</Text>
+				</Button>
 				<Text>Forgot your password?</Text>
 				<Text>or</Text>
-				<Button>Continue With Google</Button>
+				<Button mode="contained">
+					<Text>Continue With Google</Text>
+				</Button>
 			</View>
 		);
 	}
