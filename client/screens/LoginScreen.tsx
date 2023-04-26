aa9935dac1abe0d22ba67b415fdb6aaf14ff8cb0
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-26 18:50:45 -0400

    updated login screen

--- client/screens/LoginScreen.tsx
+++ client/screens/LoginScreen.tsx
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Text, View } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 
-export default class LoginScreen extends Component {
-	render() {
-		return (
-			<View className="flex-1 justify-center items-center space-y-10">
-				<View className="container mx-auto px-4">
-					<TextInput className="mb-5"> Name </TextInput>
-					<TextInput> Password </TextInput>
-				</View>
-				<Button className="" mode="contained">
-					<Text>Login</Text>
-				</Button>
-				<Text>Forgot your password?</Text>
-				<Text>or</Text>
-				<Button mode="contained">
-					<Text>Continue With Google</Text>
-				</Button>
+const LoginScreen = ({ navigation }) => {
+	const [ text, setText ] = useState('');
+
+	return (
+		<View className="flex-1 justify-center items-center space-y-10">
+			<View className="container mx-auto px-4">
+				<TextInput label="Name" className="mb-5" />
+				<TextInput label="Password" secureTextEntry right={<TextInput.Icon icon="eye" />} />
 			</View>
-		);
-	}
-}
+			<Button className="" mode="contained">
+				<Text>Login</Text>
+			</Button>
+			<Text>Forgot your password?</Text>
+			<Text>or</Text>
+			<Button mode="contained">
+				<Text>Continue With Google</Text>
+			</Button>
+		</View>
+	);
+};
+export default LoginScreen;
