58d317a7a217bc1b3377c5fb62e90ade7cef7116
Garabed96 <garonazarian09@gmail.com>
2023-04-28 00:33:27 +0700

    Getting macro-nutrients profile from USDA API

--- client/screens/LogFood.tsx
+++ client/screens/LogFood.tsx
@@ -5,12 +5,19 @@ const MyComponent = () => {
     const [foodName, setFoodName] = useState('');
     const [foodList, setFoodList] = useState([]);
 
+    const params = {
+        api_key: 'HiJgd0u4cvm1mRlNW8jizLfERifOUqcuNFFQPLWJ',
+        dataType: ["Survey (FNDDS)", "Branded"],
+        pageSize: 5,
+        pageNumber:5,
+    }
+    const apiUrl = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${foodName}&pageSize=${params.pageSize}&pageNumber=${params.pageNumber}&api_key=${params.api_key}&dataType=${params.dataType}`
     const handleSearch = async () => {
         try {
-            const foodApiKey = 'HiJgd0u4cvm1mRlNW8jizLfERifOUqcuNFFQPLWJ';
-            const response = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${foodName}&pageSize=10&pageNumber=1&api_key=${foodApiKey}`);
+            const response = await fetch(apiUrl)
             const data = await response.json();
             setFoodList(data.foods);
+            console.log("foodList: ",foodList)
         } catch (error) {
             console.error(error);
         }
@@ -20,6 +27,10 @@ const MyComponent = () => {
         return (
             <View className="border border-black p-8 m-2">
                 <Text>{item.description}</Text>
+                <Text>{item.foodNutrients[0].value}g Protein</Text>
+                <Text>{item.foodNutrients[1].value}g Fat</Text>
+                <Text>{item.foodNutrients[2].value}g Carbs</Text>
+                {/*<Text>{item.foodNutrients[0].nutrientName}</Text>*/}
             </View>
         );
     };
@@ -30,6 +41,7 @@ const MyComponent = () => {
                 value={foodName}
                 onChangeText={setFoodName}
                 placeholder="Search Food"
+                className="mb-5"
             />
             <Button title="Search" onPress={handleSearch} />
             <FlatList
