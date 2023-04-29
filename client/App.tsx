3735fd21e202103f02769b004b300c9b3159da7b
Garabed96 <garonazarian09@gmail.com>
2023-04-29 17:55:30 +0700

    Implemented Middleware logger for Redux debugging

--- client/App.tsx
+++ client/App.tsx
@@ -2,14 +2,14 @@ import {useState, useEffect} from "react";
 import NavigationComponent from "./screens/NavigationComponent";
 // REDUX
 import { Provider } from 'react-redux'
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from 'redux-logger'
 import {quickLogFoods} from "./redux/reducers/quickLogReducer";
 //  @reduxjs/toolkit BAD IDEA for Initial implementation! Do not CHANGE
-const store = createStore(quickLogFoods)
+const logger = createLogger();
+const store = createStore(quickLogFoods, applyMiddleware(logger))
 
 export default function App() {
-	const [quickLog, setQuickLog] = useState([])
-	const [counter, setCounter] = useState(0)
 	useEffect(()=>{
 		console.log(store.getState())
 	},[store])
