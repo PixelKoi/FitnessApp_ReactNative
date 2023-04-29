b60299d0be0ebd64a68961c831ca42a90e3559c2
Garabed96 <garonazarian09@gmail.com>
2023-04-29 17:34:11 +0700

    Cleanup App.tsx, created NavigationComponent.tsx for screens

--- client/App.tsx
+++ client/App.tsx
@@ -1,39 +1,21 @@
 import {useState, useEffect} from "react";
-import HomeScreen from './screens/authenticationScreens/HomeScreen';
-import LoginScreen from './screens/authenticationScreens/LoginScreen';
-import SignUpScreen from './screens/authenticationScreens/SignUpScreen';
-import ForgotPassScreen from './screens/authenticationScreens/ForgotPassScreen';
-import ApplicationNavigator from "./screens/applicationTabs/ApplicationNavigator";
-import UserBioInput from "./screens/userInfo/userBioInput";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const Stack = createNativeStackNavigator();
+import NavigationComponent from "./screens/NavigationComponent";
 // REDUX
 import { Provider } from 'react-redux'
 import { createStore } from "redux";
 import {quickLogFoods} from "./redux/reducers/quickLogReducer";
+//  @reduxjs/toolkit BAD IDEA for Initial implementation! Do not CHANGE
 const store = createStore(quickLogFoods)
+
 export default function App() {
 	const [quickLog, setQuickLog] = useState([])
 	const [counter, setCounter] = useState(0)
 	useEffect(()=>{
-		console.log(store)
+		console.log(store.getState())
 	},[store])
 	return (
 		<Provider store={store}>
-			<NavigationContainer>
-				<Stack.Navigator screenOptions={({ route }) => ({
-					headerShown: route.name !== 'ApplicationNavigator' && route.name !== 'UserBioInput',
-				})}>
-					<Stack.Screen name="Home" component={HomeScreen} />
-					<Stack.Screen name="Login" component={LoginScreen} />
-					<Stack.Screen name="SignUp" component={SignUpScreen} />
-					<Stack.Screen name="ForgotPass" component={ForgotPassScreen} />
-					<Stack.Screen name="ApplicationNavigator" component={ApplicationNavigator} />
-					<Stack.Screen name="UserBioInput" component={UserBioInput} />
-				</Stack.Navigator>
-
-			</NavigationContainer>
+			<NavigationComponent/>
 		</Provider>
 	);
 }
---
+++ client/screens/NavigationComponent.tsx
@@ -0,0 +1,25 @@
+import HomeScreen from './authenticationScreens/HomeScreen';
+import LoginScreen from './authenticationScreens/LoginScreen';
+import SignUpScreen from './authenticationScreens/SignUpScreen';
+import ForgotPassScreen from './authenticationScreens/ForgotPassScreen';
+import ApplicationNavigator from "./applicationTabs/ApplicationNavigator";
+import UserBioInput from "./userInfo/userBioInput";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
+export default function NavigationComponent() {
+    return(
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={({ route }) => ({
+            headerShown: route.name !== 'ApplicationNavigator' && route.name !== 'UserBioInput',
+        })}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+            <Stack.Screen name="ForgotPass" component={ForgotPassScreen} />
+            <Stack.Screen name="ApplicationNavigator" component={ApplicationNavigator} />
+            <Stack.Screen name="UserBioInput" component={UserBioInput} />
+        </Stack.Navigator>
+    </NavigationContainer>)
+}
