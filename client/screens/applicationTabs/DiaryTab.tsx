2b04b5bab3ea65d9eabe82c51110c9f3ae1d3d52
Garabed96 <garonazarian09@gmail.com>
2023-05-05 15:44:11 +0700

    diary calore TopBar

--- client/screens/applicationTabs/DiaryTab.tsx
+++ client/screens/applicationTabs/DiaryTab.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Divider, Text } from 'react-native-paper';
 
 const DiaryTab = (props) => {
 	console.log("passing food data to FOODDIARY:",props.route);
+	const selectedFoods = props.route
+	// TODO: set a global state for totalCalories and pass to diary
+
 	return (
-		<View className="flex-1 justify-center items-center">
-			<Text className="text-lg">This is my diary.</Text>
+		<View className="flex-1 items-center">
+				<View className="h-16 p-4 bg-black justify-center">
+					<Text className="text-white ">
+						{/*This top bar will be changed to {} destruct variables...*/}
+						Total calories - consumed calories + exercise = calories remaining
+					</Text>
+					<Text className="text-white text font-bold">
+						Goal - Food + Exercise = Remaining
+					</Text>
+				</View>
+				{/* other content */}
+			<View className="w-full mt-4">
+				<Text className="text-2xl font-bold mb-2">Breakfast</Text>
+			</View>
+			<Divider />
+
+			<View className="w-full mt-4">
+				<Text className="text-2xl font-bold mb-2">Lunch</Text>
+			</View>
+			<Divider />
+
+			<View className="w-full mt-4">
+				<Text className="text-2xl font-bold mb-2">Dinner</Text>
+			</View>
+			<Divider />
+
+			<View className="w-full mt-4">
+				<Text className="text-2xl font-bold mb-2">Snacks</Text>
+			</View>
+			<Divider />
 		</View>
 	);
 };
