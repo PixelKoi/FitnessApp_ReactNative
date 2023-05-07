9ce0ed1bd81913c9df2b48772033766e6e44d8f4
Garabed96 <garonazarian09@gmail.com>
2023-05-07 15:34:12 +0700

    cleanup and diary logging

--- client/screens/applicationTabs/Diary.tsx
+++ client/screens/applicationTabs/Diary.tsx
@@ -3,14 +3,11 @@ import { View } from 'react-native';
 import { Divider, Text } from 'react-native-paper';
 
 const Diary = (props) => {
-	// console.log("SELECTFOOD:", props.route.params)
-	// console.log("SELECTFOOD:", props.route.selectedOption)
 	const selectedObject = props.route
 	const selectedFoods = props.route.params.selectedFoods
 	const selectedOption = props.route.params.selectedOption
 	// TODO: set a global state for totalCalories and pass to diary
 	const object = props.route.params
-	// console.log(object)
 	interface Category {
 		name: string;
 		items: FoodItem[];
@@ -33,22 +30,16 @@ const Diary = (props) => {
 		{ name: "Lunch", items: [] },
 		{ name: "Dinner", items: [] },
 	];
-	// selectedFoods.map(food => {
-	// 	console.log(food.quantity , "count, calories: ", food.food.Calories)
-	// })
 	const [selectedFud, setSelectedFud] = useState();
 	const caloriesConsumed = Object.keys(selectedFoods).reduce((total, foodId) => {
 		const food = selectedFoods[foodId];
 		return total + food.food.Calories * food.quantity;
 	}, 0);
-	// console.log(caloriesConsumed)
-	// console.log("selectedOptions",typeof selectedOption, selectedOption)
 	useEffect(() => {
 		const categoryIndex = categories.findIndex((category) => category.name === selectedOption);
 		if (categoryIndex >= 0) {
 			categories[categoryIndex].items.push(...selectedFoods);
 			const populatedCategories = categories.filter(category => category.items.length > 0);
-			console.log("selectedFud", populatedCategories)
 
 			for (const obj of populatedCategories) { // loop through each object in the array
 				const items = obj.items; // get the "items" array property from the current object
@@ -64,26 +55,40 @@ const Diary = (props) => {
 		}
 	}, [props]);
 
+	const displaySelectedFud = () => {
+		console.log("selectedFud", selectedFud)
+		return (
+			<>
+				{selectedFud.map((obj) => {
+					const items = obj.items;
+					const option = obj.name;
+					return (
+						<Text key={option}>
+							empty text
+							{/* {option === mealTime ? (
+						items.map((item) => (
+							<Text key={item.id}>
+								{item.food.description} - {item.quantity} {" "}
+								{item.description}  {item.Calories * item.quantity} Calories
+							</Text>
+						))
+					) : <Text></Text>} */}
+						</Text>
+					);
+				})}
+			</>
+		);
+	};
 
 
-	// TODO: Split objects by eating time and save in global state.
-
-	// const totalCalories = Object.keys(selectedFoods).reduce((total, foodId) => {
-	// 	const food = selectedFoods[foodId];
-	// 	return total + food.food.Calories * food.quantity;
-	// }, 0);
-	// useEffect(()=>{
-	// 	console.log(totalCalories)
-	// }, [props])
 	return (
 		<View className="flex-1 items-center">
 				<View className="h-16 p-4 bg-black justify-center">
 					<Text className="text-white ">
-						{/*This top bar will be changed to {} destruct variables...*/}
-						Total calories - {caloriesConsumed} + exercise = calories remaining
+						Total calories - {caloriesConsumed} = calories remaining
 					</Text>
 					<Text className="text-white text font-bold">
-						Goal - Food + Exercise = Remaining
+						Goal - Food = Remaining
 					</Text>
 				</View>
 				{/* other content */}
@@ -97,19 +102,17 @@ const Diary = (props) => {
 			</View>
 			<View className="w-full mt-4">
 				<Text className="text-2xl font-bold mb-2">Breakfast</Text>
+				{displaySelectedFud()}
 			</View>
 			<Divider />
-
 			<View className="w-full mt-4">
 				<Text className="text-2xl font-bold mb-2">Lunch</Text>
 			</View>
 			<Divider />
-
 			<View className="w-full mt-4">
 				<Text className="text-2xl font-bold mb-2">Dinner</Text>
 			</View>
 			<Divider />
-
 			<View className="w-full mt-4">
 				<Text className="text-2xl font-bold mb-2">Snacks</Text>
 			</View>
