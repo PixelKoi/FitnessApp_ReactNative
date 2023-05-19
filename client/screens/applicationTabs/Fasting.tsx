f9eb5a451dfdcb2cceb1a23cdb14c0ad0a775596
Jonatahn Bajada <jbajada@ryerson.ca>
2023-05-19 13:54:28 -0400

    Added strokedashoffset animation to donut graph

--- client/screens/applicationTabs/Fasting.tsx
+++ client/screens/applicationTabs/Fasting.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Animated } from "react-native";
 import { PaperClipIcon } from "react-native-heroicons/outline";
 import { useNavigation } from "@react-navigation/native";
 import Svg, { G, Circle } from "react-native-svg";
+import { transparent } from "react-native-paper/lib/typescript/src/styles/themes/v2/colors";
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 const Donut = ({
 	percentage = 75,
-	radius = 40,
+	radius = 150,
 	strokeWidth = 10,
 	duration = 500,
 	color = "red",
@@ -14,23 +17,62 @@ const Donut = ({
 	textColor,
 	max = 100,
 }) => {
+	const animatedValue = React.useRef(new Animated.Value(0)).current;
+	const circleRef = React.useRef();
 	const halfCircle = radius + strokeWidth;
+	const circleCircumferance = 2 * Math.PI * radius;
+	const animation = (toValue) => {
+		return Animated.timing(animatedValue, {
+			toValue,
+			duration,
+			delay,
+			useNativeDriver: true,
+		}).start();
+	};
+
+	React.useEffect(() => {
+		animation(percentage);
+
+		animatedValue.addListener((v) => {
+			if (circleRef?.current) {
+				const maxPerc = (100 * v.value) / max;
+				const strokeDashoffset =
+					circleCircumferance - (circleCircumferance * maxPerc) / 100;
+				circleRef.current.setNativeProps({
+					strokeDashoffset,
+				});
+			}
+		});
+	});
+
 	return (
 		<View className="flex-1 justify-center items-center">
 			<Svg
 				width={radius * 2}
 				height={radius * 2}
 				viewBox={`0 0 ${halfCircle * 2} ${halfCircle * 2}`}>
-				<G>
+				<G rotation="-90" origin={`${halfCircle}, ${halfCircle}`}>
 					<Circle
 						cx="50%"
 						cy="50%"
 						stroke={color}
 						strokeWidth={strokeWidth}
 						r={radius}
+						fill="transparent"
 						strokeOpacity={0.2}
 					/>
-					<Circle />
+					<AnimatedCircle
+						ref={circleRef}
+						cx="50%"
+						cy="50%"
+						stroke={color}
+						strokeWidth={strokeWidth}
+						r={radius}
+						fill="transparent"
+						strokeDasharray={circleCircumferance}
+						strokeDashoffset={circleCircumferance}
+						strokeLinecap="round"
+					/>
 				</G>
 			</Svg>
 		</View>
