e4cdb2faadba9d936dba54eaf2ad23668cfeecb1
Garabed96 <garonazarian09@gmail.com>
2023-06-07 01:09:24 +0700

    schema updates

--- client/database/index.ts
+++ client/database/index.ts
@@ -9,10 +9,10 @@ const adapter = new SQLiteAdapter({
   schema,
   migrations,
   // (optional database name or file system path)
-  // dbName: 'myapp',
+  dbName: "myapp",
   // (recommended option, should work flawlessly out of the box on iOS. On Android,
   // additional installation steps have to be taken - disable if you run into issues...)
-  jsi: true /* Platform.OS === 'ios' */,
+  // jsi: true /* Platform.OS === 'ios' */,
   onSetUpError: (error) => {
     console.log("DB FAILED TO LOAD");
     console.log(error);
