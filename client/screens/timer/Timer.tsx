93fd8a14e136cd3a20511e6233ec0962d5771fb7
Jonatahn Bajada <jbajada@ryerson.ca>
2023-06-09 14:25:27 -0400

    added conditional css to fasting buttons

--- client/screens/timer/Timer.tsx
+++ client/screens/timer/Timer.tsx
@@ -11,7 +11,9 @@ const Timer = () => {
 			<View className="flex flex-row justify-center gap-4 mt-20">
 				<Button
 					onPress={() => setMode("fasting")}
-					className="text-center w-40 bg-button-focus text-base"
+					className={`text-center w-40 ${
+						mode === "fasting" ? "bg-primary" : "bg-secondary"
+					} text-base`}
 					icon="clock"
 					mode="contained">
 					Fasting
@@ -19,7 +21,9 @@ const Timer = () => {
 				<Button
 					onPress={() => setMode("meditation")}
 					icon="brain"
-					className="text-center w-40 bg-button-blur text-base"
+					className={`text-center w-40 ${
+						mode === "meditation" ? "bg-primary" : "bg-secondary"
+					} text-base`}
 					mode="contained">
 					Meditation
 				</Button>
