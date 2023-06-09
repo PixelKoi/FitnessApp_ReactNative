d705fc51924616bedf5a042773b4db369ca05837
Jonatahn Bajada <jbajada@ryerson.ca>
2023-06-09 14:52:26 -0400

    added fasting and med middleware back

--- client/redux-manager/store.ts
+++ client/redux-manager/store.ts
@@ -6,39 +6,40 @@ import meditationReducer from "./redux-slice/meditation-slice";
 import startFastingTimer from "./middleware/fasting-timer";
 import startMeditationTimer from "./middleware/meditation-timer";
 import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 const reducers = combineReducers({
-  user: userReducer,
-  session: sessionReducer,
-  fasting: fastingReducer,
-  meditation: meditationReducer,
+	user: userReducer,
+	session: sessionReducer,
+	fasting: fastingReducer,
+	meditation: meditationReducer,
 });
 
 const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
+	key: "root",
+	version: 1,
+	storage,
 };
-
+const fastingTimerMiddleware = startFastingTimer.middleware;
+const meditationTimerMiddleware = startMeditationTimer.middleware;
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}).concat(fastingTimerMiddleware, meditationTimerMiddleware),
 });
 
 export const persistor = persistStore(store);
