008a58b362e0e810df606e9eb25a12db1e49fb3e
Garabed96 <garonazarian09@gmail.com>
2023-06-10 15:32:41 +0700

    fix redux - persist storage

--- client/redux-manager/store.ts
+++ client/redux-manager/store.ts
@@ -6,27 +6,27 @@ import meditationReducer from "./redux-slice/meditation-slice";
 import startFastingTimer from "./middleware/fasting-timer";
 import startMeditationTimer from "./middleware/meditation-timer";
 import {
-	persistStore,
-	persistReducer,
-	FLUSH,
-	REHYDRATE,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const reducers = combineReducers({
-	user: userReducer,
-	session: sessionReducer,
-	fasting: fastingReducer,
-	meditation: meditationReducer,
+  user: userReducer,
+  session: sessionReducer,
+  fasting: fastingReducer,
+  meditation: meditationReducer,
 });
 
 const persistConfig = {
-	key: "root",
-	version: 1,
-	storage,
+  key: "root",
+  version: 1,
+  storage: AsyncStorage,
 };
 
 //Custom Middleware
@@ -36,13 +36,13 @@ const meditationTimerMiddleware = startMeditationTimer.middleware;
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-	reducer: persistedReducer,
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}).concat(fastingTimerMiddleware, meditationTimerMiddleware),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(fastingTimerMiddleware, meditationTimerMiddleware),
 });
 
 export const persistor = persistStore(store);
