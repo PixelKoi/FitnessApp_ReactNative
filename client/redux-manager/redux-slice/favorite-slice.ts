794522e932be2f92de39511f0d17a982788d46cc
Garabed96 <garonazarian09@gmail.com>
2023-06-18 03:29:11 +0700

    Array working with TS

--- client/redux-manager/redux-slice/favorite-slice.ts
+++ client/redux-manager/redux-slice/favorite-slice.ts
@@ -1,31 +1,34 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { values } from "@nozbe/watermelondb/utils/fp";
 
 // Define the type for the favorite state
-interface FavoriteState {
+interface Favorite {
   Calories: number;
   Carbs: number;
   Fat: number;
   Protein: number;
   description: string;
   id: number;
+  fav_id: number;
 }
 
-const initialState = {
-  foods: [],
+// Our FavoriteState is going to have a list of Favorites
+interface FavoriteState {
+  favorites: Favorite[];
+}
+
+const initialState: FavoriteState = {
+  favorites: [],
 };
 
 const favSlice = createSlice({
   name: "favorite",
   initialState,
   reducers: {
-    addFavorite(state, action: PayloadAction<FavoriteState>) {
-      const newFavorite = {
+    addFavorite: (state, action: PayloadAction<Favorite>) => {
+      state.favorites.push({
+        fav_id: state.favorites.length + 1,
         ...action.payload,
-        fav_id: state.length + 1, // Generate a unique ID based on the current length
-      };
-      state.push(newFavorite);
+      });
     },
   },
 });
