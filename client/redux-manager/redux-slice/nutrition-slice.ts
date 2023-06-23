5628650f5b6252f30bd92c3c79f3c2d9980f9cef
Garabed96 <garonazarian09@gmail.com>
2023-06-23 17:51:40 +0700

    nutrition-slice.ts reducers

--- client/redux-manager/redux-slice/nutrition-slice.ts
+++ client/redux-manager/redux-slice/nutrition-slice.ts
@@ -20,14 +20,47 @@ const initialState: InventoryState = {
   inventory: [],
 };
 
+// REDUCERS
+// 1. addInventory: id EXISTS && quantity > 0, INCREMENT quantity ELSE id !EXISTS push obj to array
+// 2. reduceInventory: ID Match && quantity > 1, DECREMENT quantity ELSE quantity = 0 PROC deleteInventory
+// 3. deleteInventory: Remove ID Match from inventory ARRAY
 const nutritionSlice = createSlice({
   name: "inventory",
   initialState,
   reducers: {
     addInventory: (state, action: PayloadAction<Nutrition>) => {
-      state.inventory.push({
-        ...action.payload,
-      });
+      const itemIndex = state.inventory.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex !== -1 && action.payload.quantity > 0) {
+        state.inventory[itemIndex].quantity += action.payload.quantity;
+      } else if (itemIndex === -1) {
+        state.inventory.push(action.payload);
+      }
+    },
+    reduceInventory: (state, action: PayloadAction<number>) => {
+      const itemIndex = state.inventory.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      if (itemIndex !== -1 && state.inventory[itemIndex].quantity > 1) {
+        state.inventory[itemIndex].quantity -= 1;
+      } else if (
+        itemIndex !== -1 &&
+        state.inventory[itemIndex].quantity === 1
+      ) {
+        state.inventory.splice(itemIndex, 1);
+      }
+    },
+    deleteInventory: (state, action: PayloadAction<number>) => {
+      const itemIndex = state.inventory.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      if (itemIndex !== -1) {
+        state.inventory.splice(itemIndex, 1);
+      }
     },
   },
 });
