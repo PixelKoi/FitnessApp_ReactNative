fe2478d525b1268a2a83e085efe373509d037ae4
Jonatahn Bajada <jbajada@ryerson.ca>
2023-06-22 23:30:56 -0400

    updated card css

--- client/screens/Timers/Meditation/MeditationSession.tsx
+++ client/screens/Timers/Meditation/MeditationSession.tsx
@@ -12,7 +12,7 @@ const MeditationSession = (props) => {
 				backgroundColor: props.cardColor,
 				borderRadius: 10,
 			}}>
-			<View className="flex-col justify-center gap-1 mr-8 mt-14">
+			<View className="flex-col justify-center  mt-14">
 				<Text className="font-bold" style={{ color: props.textColor }}>
 					{props.title}
 				</Text>
@@ -21,23 +21,30 @@ const MeditationSession = (props) => {
 				</Text>
 			</View>
 
-			<View className="flex-row gap-5 mt-auto mb-4">
-				<Text className="font-medium" style={{ color: props.textColor }}>
-					{props.time} MIN
-				</Text>
-				<TouchableWithoutFeedback
-					style={{
-						width: 45,
-						height: 20,
-						backgroundColor: props.butttonBackgroundColor,
-						borderRadius: 10,
-					}}>
+			<View className="flex-row items-center mt-auto mb-4">
+				<View style={{ width: 67.5 }}>
 					<Text
-						style={{ fontSize: 10, color: props.buttonTextColor }}
-						className="self-center my-auto font-semibold">
-						Start
+						className="ml-2 font-medium self-center"
+						style={{ color: props.textColor }}>
+						{props.time} MIN
 					</Text>
-				</TouchableWithoutFeedback>
+				</View>
+				<View style={{ width: 67.5 }}>
+					<TouchableWithoutFeedback
+						className="self-center mr-2"
+						style={{
+							width: 45,
+							height: 20,
+							backgroundColor: props.butttonBackgroundColor,
+							borderRadius: 10,
+						}}>
+						<Text
+							style={{ fontSize: 10, color: props.buttonTextColor }}
+							className="self-center my-auto font-semibold">
+							Start
+						</Text>
+					</TouchableWithoutFeedback>
+				</View>
 			</View>
 		</TouchableOpacity>
 	);
