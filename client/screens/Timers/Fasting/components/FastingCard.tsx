e407b528a7762d1a7669b07d7e4522402e858ee3
Jonatahn Bajada <jbajada@ryerson.ca>
2023-06-28 18:54:17 -0400

    updated fasting card css

--- client/screens/Timers/Fasting/components/FastingCard.tsx
+++ client/screens/Timers/Fasting/components/FastingCard.tsx
@@ -4,70 +4,100 @@ import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import { Surface } from "react-native-paper";
 import { useAppSelector } from "../../../../redux-manager/hooks";
 import CircadianChicken from "../../../../assets/images/fasting-dash/CircadianChicken.png";
+import ClockFast from "../../../../assets/images/fasting-dash/ClockFast.png";
+import BurgerFast from "../../../../assets/images/fasting-dash/BurgerFast.png";
 
 const FastingCard = (props) => {
 	const { colors } = useAppSelector((state) => state.theme);
 	return (
 		<View className="pb-5">
 			<Surface
-				className="flex-row"
+				className="flex-row items-center justify-center"
 				style={{
-					height: 150,
+					height: 130,
 					width: 300,
 					backgroundColor: colors.secondary,
 					borderRadius: 20,
 				}}>
-				<View style={{ width: 150 }}>
-					<View className="flex-row ">
-						<Image source={CircadianChicken} />
+				<Image source={CircadianChicken} className="mr-3" />
 
-						<View>
-							<View className="">
+				<View className="">
+					<View className="">
+						<Text
+							style={{ fontSize: 18, color: colors.primary }}
+							className="font-bold">
+							16:8 Intermittent
+						</Text>
+					</View>
+					<View className="gap-2 mt-1">
+						<View className="flex-row">
+							<View
+								className="my-auto"
+								style={{
+									backgroundColor: "transparent",
+									width: 20,
+									height: 20,
+								}}>
+								<Image source={ClockFast} style={{ width: 20 }} />
+							</View>
+							<View
+								className="ml-1 opacity-50 rounded-full"
+								style={{
+									backgroundColor: colors.primary,
+									width: 70,
+									height: 20,
+								}}>
 								<Text
-									style={{ fontSize: 18, color: props.textColor }}
-									className="font-bold">
-									16:8 Intermittent
+									style={{ color: "#ffff", fontSize: 10 }}
+									className="self-center my-auto">
+									20 HOURS
 								</Text>
 							</View>
-							<View className="gap-1">
-								<View
-									className="opacity-60 rounded-full"
-									style={{ backgroundColor: colors.primary, width: 80 }}>
-									<Text style={{ color: "#ffff" }} className="self-center">
-										20 HOURS
-									</Text>
-								</View>
-								<View
-									className="opacity-60 rounded-full"
-									style={{ backgroundColor: colors.primary, width: 80 }}>
-									<Text style={{ color: "#ffff" }} className="self-center">
-										4 HOURS
-									</Text>
-								</View>
+						</View>
+						<View className="flex-row">
+							<View
+								className="my-auto"
+								style={{
+									backgroundColor: "transparent",
+									width: 20,
+									height: 20,
+								}}>
+								<Image
+									source={BurgerFast}
+									className="my-auto"
+									style={{ width: 20 }}
+								/>
+							</View>
+							<View
+								className="ml-1 opacity-50 rounded-full"
+								style={{
+									backgroundColor: colors.primary,
+									width: 70,
+									height: 20,
+								}}>
+								<Text
+									style={{ color: "#ffff", fontSize: 10 }}
+									className="self-center my-auto">
+									4 HOURS
+								</Text>
 							</View>
 						</View>
 					</View>
-					{/* <View className="p-5 bottom-3">
-						<View
-							style={{
-								width: 60,
-								height: 30,
-								backgroundColor: props.buttonBackgroundColor,
-							}}
-							className="rounded-full">
-							<Text
-								style={{ color: "#ffff" }}
-								className="font-semibold my-auto text-center">
-								Start
-							</Text>
-						</View>
-					</View> */}
 				</View>
-
-				<View style={{ width: 150 }} className=" items-center self-center">
-					<Image
-						style={{ height: props.height, width: props.width }}
-						source={props.img}></Image>
+				<View className="top-10 right-1">
+					<View
+						style={{
+							width: 50,
+							height: 20,
+							backgroundColor: colors.primary,
+						}}
+						className="rounded-full">
+						<Text
+							style={{ color: "#ffff", fontSize: 10 }}
+							className="font-semibold my-auto text-center">
+							Start
+						</Text>
+					</View>
 				</View>
 			</Surface>
 		</View>
