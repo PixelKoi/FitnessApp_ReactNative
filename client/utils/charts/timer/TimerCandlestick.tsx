4102ae181fdbd7097d087d7db13072cf28bf14c0
Garabed96 <garonazarian09@gmail.com>
2023-07-13 01:48:40 +0700

    TimerCandlestick.tsx xy-axis fixed

--- client/utils/charts/timer/TimerCandlestick.tsx
+++ client/utils/charts/timer/TimerCandlestick.tsx
@@ -1,39 +1,27 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
-import { VictoryChart, VictoryAxis, VictoryCandlestick } from "victory-native";
-import { Dimensions } from "react-native";
+import { View } from "react-native";
+import { VictoryChart, VictoryAxis } from "victory-native";
 
 function TimerCandlestick({}) {
-  const sampleData = [
-    { x: 1, y: 6 }, // Monday: 6 hours
-    { x: 2, y: 12 }, // Tuesday: 12 hours
-    { x: 3, y: 18 }, // Wednesday: 18 hours
-    // ...and so on for the other days
-  ];
-
   return (
     <View>
-      <VictoryChart
-        domainPadding={{ x: 25, y: 0 }}
-        scale={{ x: "linear", y: "linear" }}
-      >
+      <VictoryChart domain={{ y: [0, 24] }} scale={{ x: "time", y: "linear" }}>
         <VictoryAxis
-          orientation="left" // Display the axis on the left side
-          tickValues={[0, 6, 12, 18, 24]} // Specify tick values for the y-axis
-          tickFormat={(tick) => `${tick}h`} // Format tick labels as hours
+          tickValues={[24, 18, 12, 6, 0]}
+          tickFormat={(t) => `${t}h`}
+          dependentAxis
           style={{
-            axis: { stroke: "transparent" }, // Hide the axis line
-            ticks: { stroke: "transparent" }, // Hide the ticks
+            axis: { stroke: "transparent" },
+            ticks: { stroke: "transparent" },
           }}
         />
         <VictoryAxis
-          tickValues={[1, 2, 3, 4, 5, 6, 7]} // Specify tick values for the x-axis
-          tickFormat={["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]} // Specify tick labels for the x-axis
-          orientation="bottom"
-        />
-        <VictoryCandlestick
-          candleColors={{ positive: "#5f5c5b", negative: "#c43a31" }}
-          data={sampleData}
+          tickValues={[1, 2, 3, 4, 5, 6, 7]}
+          tickFormat={["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]}
+          style={{
+            axis: { stroke: "transparent" },
+            ticks: { stroke: "transparent" },
+          }}
         />
       </VictoryChart>
     </View>
