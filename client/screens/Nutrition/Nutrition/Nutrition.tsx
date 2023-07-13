62845e0445e1baafd6e349a49c88ea71e7c676c4
Garabed96 <garonazarian09@gmail.com>
2023-07-13 08:44:23 +0700

    cleanup

--- client/screens/Nutrition/Nutrition/Nutrition.tsx
+++ client/screens/Nutrition/Nutrition/Nutrition.tsx
@@ -72,18 +72,10 @@ const styles = StyleSheet.create({
 });
 
 const Nutrition: React.FC = (props) => {
-  useEffect(() => {
-    if (props.route && props.route.params && props.route.params.meal) {
-      console.log("MEAL: ", props.route.params.meal);
-      setSelectedOption(props.route.params.meal);
-    }
-  }, [props.route && props.route.params ? props.route.params.meal : undefined]);
-
   const dispatch = useAppDispatch();
   const { colors } = useAppSelector((state) => state.theme);
   const { dailyCal } = useAppSelector((state) => state.user);
-  console.log("PROFILE CALS: ", dailyCal);
-  console.log("colors:", colors.primary);
+
   // ACCESS THEME COLORS
   // const primary_color = colors.primary;
   const primary_color = colors.primary;
@@ -151,7 +143,6 @@ const Nutrition: React.FC = (props) => {
 
   const [visible, setVisible] = useState(false);
   const [selectedOption, setSelectedOption] = useState("");
-  const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
   const [mealError, setMealError] = useState(false);
   // let redux-slice submit quick log when state selectedOption set
@@ -269,6 +260,13 @@ const Nutrition: React.FC = (props) => {
     const updatedItem = { ...item, quantity: item.quantity - 1 };
     dispatch(reduceInventory(updatedItem));
   };
+
+  useEffect(() => {
+    if (props.route && props.route.params && props.route.params.meal) {
+      console.log("MEAL: ", props.route.params.meal);
+      setSelectedOption(props.route.params.meal);
+    }
+  }, [props.route && props.route.params ? props.route.params.meal : undefined]);
 
   useEffect(() => {
     const loggedFoods = inventory.filter((food) => food.quantity > 0);
